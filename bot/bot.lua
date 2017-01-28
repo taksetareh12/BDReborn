ce03ffc04f8674a443e3031db674e4d846485551
samuel <ground_zeroo@yahoo.com>
2017-01-28 05:00:48 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,7 +1,8 @@
 tdcli = dofile('./tg/tdcli.lua')
 serpent = (loadfile "./libs/serpent.lua")()
 feedparser = (loadfile "./libs/feedparser.lua")()
-our_id = 265541133 -- Put Here Your Bot ID
+our_id =317989507
+First: mogtaba- Put Here Your Bot ID
 --ایدی رباتتونو اینجا بزارید
 URL = require "socket.url"
 http = require "socket.http"
@@ -84,7 +85,8 @@ function create_config( )
     "plugins",
     "tools"
  },
-    sudo_users = {157059515},
+    sudo_users = {317989507
+First: mogtaba},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
